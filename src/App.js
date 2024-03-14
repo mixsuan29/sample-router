42d645ce6d94b5d2edd3493b698216a5bfaf6b84
mixsuan29 <mixsuan29@gmail.com>
2024-03-14 20:50:15 +0800

    Navbar元件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <div className="container">
+        主要內容輸出的地方
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (<nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    )
+}
