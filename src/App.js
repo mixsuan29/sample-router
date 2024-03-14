433131ee1c6ce7d196759117606ee9da2e4ce891
mixsuan29 <mixsuan29@gmail.com>
2024-03-14 21:17:41 +0800

    加入路由 Routes, Route, Link，頁面切換

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="container">
-        主要內容輸出的地方
+      <div className="container mt-3">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
     return (<nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
@@ -5,7 +7,10 @@ export default function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <Link className="nav-link" to='/'>HOME</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/about'>ABOUT</Link>
                 </li>
             </ul>
             </div>
